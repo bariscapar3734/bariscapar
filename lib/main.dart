217421b0fced0a658be267dad02bd8c1ec94547e
bariscapar <bariscapar6@gmail.com>
2021-04-28 01:28:58 +0300

    uygulama adı ve iconu değiştirildi

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ import 'login.dart';
 void main() {
   runApp(
     MaterialApp(
+      title: "Ülkeleri Tanıyalım",
       debugShowCheckedModeBanner: false,
       initialRoute: "/",
       routes: {
